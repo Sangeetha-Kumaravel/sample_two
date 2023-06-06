4baddeb03eaca6dbe22a599eeba44cbd47d6c67b
Sangeetha-Kumaravel <sangeethavelu12@gmail.com>
2023-06-06 21:41:11 +0530

    ver 2 commit

--- classspringdemo/src/main/java/com/classspringdemo/inheritance/practiceThree.java
+++ classspringdemo/src/main/java/com/classspringdemo/inheritance/practiceThree.java
@@ -2,8 +2,8 @@ package com.classspringdemo.inheritance;
 
 class practiceThree {
 	
-	int a = 10;
-	static int b = 20;
+	int a = 100;
+	static int b = 200;
 	static void m1() {
 		
 		System.out.println("super class m1 method");
@@ -17,8 +17,8 @@ class practiceThree {
 
 class practiceFour extends practiceThree {
 	
-	int a = 30;
-	static int b = 40;
+	int a = 300;
+	static int b = 400;
 	static void m1() {
 		
 		System.out.println("sub class m1 method");
